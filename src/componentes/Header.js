0b284942ea0430e59d8ecb41613ae402b0bf8f0c
davi.viana <davi.viana@dtidigital.com.br>
2019-02-12 23:06:05 -0200

    Initial layout with components header and timeline

---
+++ src/componentes/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+
+    render = () => {
+    return (
+        <div>
+            <header className="header container">
+                <h1 className="header-logo">Instalura</h1>
+
+                <form lpformnum="1" className="header-busca">
+                <input type="text" name="search" placeholder="Pesquisa" className="header-busca-campo"/>
+                <input type="submit" value="Buscar" className="header-busca-submit" />
+                </form>
+
+                <nav>
+                <ul className="header-nav">
+                    <li className="header-nav-item">
+                    </li>
+                </ul>
+                </nav>
+            </header>
+        </div>
+    );
+  }
+}
