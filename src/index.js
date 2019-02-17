3e2f3b2c2351a29ce4126dafc41138d927c75622
davi.viana <davi.viana@dtidigital.com.br>
2019-02-17 12:34:58 -0300

    Login component creation

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import * as serviceWorker from './serviceWorker';
 
 import './css/reset.css';
 import './css/timeline.css'
+import './css/login.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
