f66e21252be292a83d3c3139a46b59427566fc45
davi.viana <davi.viana@dtidigital.com.br>
2019-03-03 10:15:30 -0300

    Start using redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {createStore} from 'redux';
+
 import Header from './componentes/Header';
 import Timeline from './componentes/Timeline';
 
@@ -6,13 +8,25 @@ import TimelineStore from './stores/TimelineStore';
 
 const timelineStore = new TimelineStore([]);
 
+// REDUCER
+function timeline(state = [], action) {
+  if(action.type === 'LIST') {
+    console.log('action list');
+    return action.photos;
+  }
+
+  return state;
+}
+
+const store = createStore(timeline);
+
 class App extends Component {
   render() {
     return (
       <div id="root">
         <div className="main">
           <Header />
-          <Timeline login={this.props.params.login} store={timelineStore}/>
+          <Timeline login={this.props.params.login} store={store}/>
       </div>
     </div>
     );
--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -20,8 +20,8 @@ export default class Timeline extends Component {
     }
 
     componentWillMount = () => {
-        this.props.store.subscribe(photos => {
-            this.setState({photos});
+        this.props.store.subscribe(() => {
+            this.setState({photos:this.props.store.getState()});
         })
     }
 
@@ -33,7 +33,11 @@ export default class Timeline extends Component {
         else
             url = `http://localhost:8080/api/public/fotos/${props.login}`;
 
-        this.props.store.loadPhotos(url);
+        const listaFixa = [{"urlPerfil":"https://instagram.fcgh9-1.fna.fbcdn.net/vp/960227fa1524bee9e36610f8da71889c/5B6F42E1/t51.2885-19/11199408_569104449895751_1837574990_a.jpg","loginUsuario":"alots","horario":"17/02/2019 11:01","urlFoto":"https://instagram.fcgh10-1.fna.fbcdn.net/t51.2885-15/e35/14482111_1635089460122802_8984023070045896704_n.jpg?ig_cache_key=MTM1MzEzNjM4NzAxMjIwODUyMw%3D%3D.2","id":1,"likeada":false,"likers":[],"comentarios":[],"comentario":"comentario da foto"},{"urlPerfil":"https://instagram.fcgh9-1.fna.fbcdn.net/vp/960227fa1524bee9e36610f8da71889c/5B6F42E1/t51.2885-19/11199408_569104449895751_1837574990_a.jpg","loginUsuario":"alots","horario":"17/02/2019 11:01","urlFoto":"https://instagram.fcgh9-1.fna.fbcdn.net/t51.2885-15/e35/15276770_381074615568085_8052939980646907904_n.jpg?ig_cache_key=MTM5ODY4MDMyNjYyMDA1MDE4OQ%3D%3D.2","id":2,"likeada":false,"likers":[],"comentarios":[],"comentario":"comentario da foto"}];
+
+        this.props.store.dispatch({type:'LIST', photos:listaFixa});
+
+        //this.props.store.loadPhotos(url);
     }
     
     likePhoto = (photoId) => {
